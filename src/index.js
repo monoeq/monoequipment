968a84c5c0982acd699c154cb74292ee677e18c2
Jon Gacnik <jon@folderstudio.com>
2018-04-29 19:09:47 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,104 @@
+var choo = require('choo')
+var html = require('choo/html')
+var css = require('sheetify')
+
+css('ress')
+css('gr8')
+
+css`
+  @font-face {
+    font-family: 'StandardBook';
+    src: url('assets/standard-book-webfont.woff2') format('woff2'),
+         url('assets/standard-book-webfont.woff') format('woff');
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-family: 'StandardBook', Arial, Helvetica, sans-serif;
+  }
+
+  h1, h2, h3, h4, h5, h6, h7 {
+    font-size: inherit;
+    font-weight: inherit;
+    font-style: inherit;
+  }
+
+  button, input {
+    outline: none;
+  }
+
+  ul, ol, li { 
+    list-style: none;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: inherit;
+  }
+
+  img, video {
+    width: 100%;
+    height: auto;
+  }
+
+  .bb1-white {
+    border-bottom: 1px solid white;
+  }
+
+  /*.rows > * {
+    border-top: 1px dashed black;
+  }
+
+  .rows > *:last-child {
+    border-bottom: 1px dashed black;
+  }*/
+
+  a:hover {
+    display: inline-block;
+    transform: rotateY(180deg);
+  }
+
+  .external {
+    position: relative;
+  }
+  .external:after {
+    content: ' ➚';
+    display: block;
+    position: absolute;
+    right: -1em;
+    top: 0;
+    pointer-events: none;
+  }
+`
+
+var app = choo()
+
+function mainview () {
+  return html`
+    <body class="x xjc xac xdc vhmn100 p1 fs2-4 tac rows psr" sm="fs3-2">
+      <h1 class="p1 s1">
+        monoequipment
+      </h1>
+      <p class="p1 s1">
+        opinionated <a href="https://github.com/choojs/nanocomponent" target="_blank">nanocomponents</a> for websites
+      </p>
+      <ul class="p1 s1">
+        <li><a class="external" target="_blank" href="https://github.com/jongacnik/monolazy">monolazy</a></li>
+        <li><a class="external" target="_blank" href="https://github.com/jongacnik/monoimage">monoimage</a></li>
+        <li><a class="external" target="_blank" href="https://github.com/jongacnik/monocontextual">monocontextual</a></li>
+      </ul>
+      <p class="p1 s1">
+        more soon
+      </p>
+    </body>
+  `
+}
+
+app.route('/', mainview)
+
+app.mount('body')
